0278f279be9a7a315b0cb5a48db289b94ad6482d
AbiPEGA <72097606+AbiPEGA@users.noreply.github.com>
2020-11-11 22:57:06 -0600

    Add files via upload

---
+++ fi/poo/POOP6V2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.poo;
+
+/**
+ *
+ * @author Abigail
+ */
+public class POOP6V2  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Yo soy la clase POOP6V2");
+    }
+
+}
