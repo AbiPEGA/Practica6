173b067be74e720276655721e13ed075bd6b5c94
AbiPEGA <72097606+AbiPEGA@users.noreply.github.com>
2020-11-11 23:08:43 -0600

    Add files via upload

---
+++ POOP6/src/poop6/POOP6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poop6;
+
+/**
+ *
+ * @author Abigail
+ */
+public class POOP6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Yo soy la clase POOP6");
+    }
+    
+}
